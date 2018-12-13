65b335e451a02be145f4966ccc52e5c1aa6d400e
Raul Ferri <rferribisquert@gmail.com>
2018-12-13 13:00:04 +0100

    Add files via upload

---
+++ src/main/java/com/sopra/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.sopra.springboot.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter 
+{
+	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception
+	{
+		
+	}
+}
