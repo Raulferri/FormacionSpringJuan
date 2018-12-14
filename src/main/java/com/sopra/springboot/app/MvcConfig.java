8d1f9ca0ac24854fa8baa3df3f7899d982f785c6
Arranque 4 <Arranque 4@192.168.5.106>
2018-12-14 08:53:56 +0100

    1234

--- src/main/java/com/sopra/springboot/app/MvcConfig.java
+++ src/main/java/com/sopra/springboot/app/MvcConfig.java
@@ -1,10 +1,14 @@
 package com.sopra.springboot.app;
 
+import org.springframework.context.annotation.Bean;
+
 //import java.nio.file.Paths;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
@@ -26,6 +30,14 @@ public class MvcConfig implements WebMvcConfigurer {
 		.addResourceLocations(resourcePath);
 		
 	}*/
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error_403").setViewName("error_403");
+	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 }
--- src/main/java/com/sopra/springboot/app/controllers/LoginController.java
+++ src/main/java/com/sopra/springboot/app/controllers/LoginController.java
@@ -10,23 +10,24 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
-	
+
 	@GetMapping("/login")
 	public String login(@RequestParam(value="error", required=false) String error,
-		                @RequestParam(value="logout", required=false) String logout,
-		                Model model, Principal principal, RedirectAttributes flash) {
+			@RequestParam(value="logout", required = false) String logout,
+			Model model, Principal principal, RedirectAttributes flash) {
 		
 		if(principal != null) {
-			flash.addFlashAttribute("info", "Usted ya ha inciado la sesión");
+			flash.addFlashAttribute("info", "Ya ha inciado sesión anteriormente");
 			return "redirect:/";
 		}
 		
 		if(error != null) {
-			model.addAttribute("error", "Error en login, nombre de usuario o contraseña incorrecta, por favor vuelva a intentarlo");
+			model.addAttribute("error", "Error en el login: Nombre de usuario o contraseña incorrecta, por favor vuelva a intentarlo!");
 		}
 		
-		if(logout != null)
-			model.addAttribute("success", "Nos vamos a casa");
+		if(logout != null) {
+			model.addAttribute("success", "Ha cerrado sesión con éxito!");
+		}
 		
 		return "login";
 	}
