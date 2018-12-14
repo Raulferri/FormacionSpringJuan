5a1551f7579e7b9c85771c4ff86d1afce1e23188
Arranque 4 <Arranque 4@192.168.5.106>
2018-12-14 10:20:08 +0100

    1234

--- src/main/java/com/sopra/springboot/app/MvcConfig.java
+++ src/main/java/com/sopra/springboot/app/MvcConfig.java
@@ -1,5 +1,7 @@
 package com.sopra.springboot.app;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 
 //import java.nio.file.Paths;
@@ -8,14 +10,17 @@ import org.springframework.context.annotation.Bean;
 //import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 //import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
-
+public class MvcConfig implements WebMvcConfigurer 
+{
 	/*private final Logger log = LoggerFactory.getLogger(getClass());
 	
 	@Override
@@ -31,13 +36,39 @@ public class MvcConfig implements WebMvcConfigurer {
 		
 	}*/
 	
-	public void addViewControllers(ViewControllerRegistry registry) {
+	public void addViewControllers(ViewControllerRegistry registry) 
+	{
 		registry.addViewController("/error_403").setViewName("error_403");
 	}
 
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
+	public BCryptPasswordEncoder passwordEncoder() 
+	{
 		return new BCryptPasswordEncoder();
 	}
 	
+	//El siguiente bloque sirve para internacionalizar
+	@Bean
+	public LocaleResolver localeResolver()
+	{
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es", "ES"));
+		
+		return localeResolver;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor()
+	{
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		
+		return localeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) 
+	{
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 }
